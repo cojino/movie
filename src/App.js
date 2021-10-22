53924945278b80454ca616106932745126a46656
cojino <aymanelgad65@gmail.com>
2021-10-22 11:42:48 +0100

    some folders in some dependencies added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement } from "./actions";
 
 function App() {
-  const dispatch = useDispatch();
-  const counter = useSelector((state) => state.counter);
-  const isLogged = useSelector((state) => state.isLogged);
-
-  return (
-    <div className="App">
-      <p>counter {counter}</p>
-      <button onClick={() => dispatch(decrement())}>-</button>
-      <button onClick={() => dispatch(increment())}>+</button>
-      {isLogged ? <p>hakona matata</p> : <p>fk you</p>}
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
